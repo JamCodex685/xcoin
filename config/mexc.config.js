5b11fb0b095845e81b3190f1d79de61f05c78587
JamCodex685 <blakecsci@gmail.com>
2024-05-09 23:53:06 -0400

    MEXC Paper Config

--- config/mexc.config.js
+++ config/mexc.config.js
@@ -5,19 +5,13 @@ module.exports = {
       script: "./xcoin.js",
       cron_restart: "50 */6 * * *",
       restart_delay: 100,
-      args: "server mexc --conf ./data/config/mexc/demo1.json",
+      args: "server mexc --conf ./data/config/mexc/mexc-test-paper.json",
     },
     {
-      name: "demo1",
+      name: "MEXC Test PAPER",
       script: "./xcoin.js",
-      watch: ["./data/pm2/restart_mexc_demo1.json"],
-      args: "trade mexc --conf ./data/config/mexc/demo1.json --with_server",
-    },
-    {
-      name: "demo2",
-      script: "./xcoin.js",
-      watch: ["./data/pm2/restart_mexc_demo2.json"],
-      args: "trade mexc --conf ./data/config/mexc/demo2.json --with_server",
+      watch: ["./data/pm2/restart_mexc_mexc-test-paper.json"],
+      args: "trade mexc --conf ./data/config/mexc/mexc-test-paper.json --with_server",
     },
   ],
 };
